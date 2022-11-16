036fd5d0a1291798a9747e779e9e40c7ef3badd5
wh015n3w <akunbaruwisnu@gmail.com>
2022-11-16 11:04:51 +0700

    [update tugas] Loop Pyramid, Prime, Fibonacci, Form HTML & Table HTML (DOM)

---
+++ tugas/html/02-table/main.js
@@ -0,0 +1,40 @@
+let dataFav = [
+  {
+    nama: "Wisnu",
+    Makanan: "Soto",
+    Minuman: "Air Putih",
+    Diaduk: "Iya",
+  },
+  {
+    nama: "Farid",
+    Makanan: "Roti",
+    Minuman: "Air Zam Zam",
+    Diaduk: "Tidak",
+  },
+  {
+    nama: "Budi",
+    Makanan: "Nasi Goreng",
+    Minuman: "Air Putih",
+    Diaduk: "Iya",
+  },
+];
+
+let judul = ["Favorite", "Orang"];
+let table = document.getElementById("myTable");
+let judulbgt = document.getElementById("judul");
+judulbgt.innerHTML = judul[0];
+
+buildTable = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    let row = `<tr>
+                  <td>${data[i].nama}</td>
+                  <td>${data[i].Makanan}</td>
+                  <td>${data[i].Minuman}</td>
+                  <td>${data[i].Diaduk}</td>
+               </tr>`;
+
+    table.innerHTML += row;
+  }
+};
+
+buildTable(dataFav);
