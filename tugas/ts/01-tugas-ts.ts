bcf152f1cad39e888f781373102f377784779139
wh015n3w <akunbaruwisnu@gmail.com>
2022-11-17 14:48:33 +0700

    [update tugas] Function Typescript

--- tugas/ts/01-tugas-ts.ts
+++ tugas/ts/01-tugas-ts.ts
@@ -1,5 +1,10 @@
 let numbers: Array<number> = new Array(1, 1, 1, 3, 3, 2);
-function count(numbers: Array<number>, target: number): number {
-  return 0;
+
+function count(numbers: Array<number>, target: number) {
+  let newNumber: number = numbers
+    .filter((a: number) => a % 2 != 0)
+    .map((a: number) => a * target)
+    .reduce((cum: number, cur: number) => cum + cur);
+  return newNumber;
 }
-console.log(count(numbers, 1));
+console.log(count(numbers, 10));
--- tugas/ts/build/01-tugas-ts.js
+++ tugas/ts/build/01-tugas-ts.js
@@ -1,6 +1,10 @@
 "use strict";
 let numbers = new Array(1, 1, 1, 3, 3, 2);
 function count(numbers, target) {
-    return 0;
+    let newNumber = numbers
+        .filter((a) => a % 2 != 0)
+        .map((a) => a * target)
+        .reduce((cum, cur) => cum + cur);
+    return newNumber;
 }
-console.log(count(numbers, 1));
+console.log(count(numbers, 10));
